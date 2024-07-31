b3ffa0162982751df2e489a64c8256a3c2253f31
Fleege <76089935+pfleege@users.noreply.github.com>
2024-07-31 14:03:53 +0300

    ADD functionality to the AddBlog component
    - Use the POST method to add the blog to the json db-file
    - Add setTimeout to simulate server delay when saving content to db
    - Add useState isSaving to disable the Add Blog button during save
    - Add the useNavigate to get the user back to the main page after save

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,14 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const AddBlog = () => {
+  const [title, setTitle] = useState("");
+  const [blogText, setBlogText] = useState("");
+  const [author, setAuthor] = useState("Samuel L. Blogson");
+  const [isSaving, setIsSaving] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    setIsSaving(true);
+
+    const url = "http://localhost:8000/blogs";
+    const newBlog = { title, blogText, author };
+
+    setTimeout(() => {
+      fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newBlog),
+        mode: "cors",
+      }).then(() => {
+        setIsSaving(false);
+      });
+    }, 1000);
+    navigate("/");
+  };
+
   return (
     <div className="blogTemplate">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(evt) => setTitle(evt.target.value)}
+        />
         <label>Blog body:</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={blogText}
+          onChange={(evt) => setBlogText(evt.target.value)}
+        ></textarea>
         <label>Blog author:</label>
-        <select>
+        <select value={author} onChange={(evt) => setAuthor(evt.target.value)}>
           <option value="Samuel L. Blogson">Samuel L Blogson</option>
           <option value="Vincent">Vincent the Man</option>
           <option value="Mr. Wolf">Mr. Wolf</option>
@@ -16,7 +54,12 @@ const AddBlog = () => {
           <option value="Mia">Mia Wallace</option>
           <option value="Mr. Glass">Mr. Glass</option>
         </select>
-        <button>Add Blog</button>
+        {!isSaving && <button>Add Blog</button>}
+        {isSaving && (
+          <button className="disabled" disabled>
+            Saving blog...
+          </button>
+        )}
       </form>
     </div>
   );
