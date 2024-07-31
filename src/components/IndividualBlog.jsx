b7edee4681f04091da8fe2587fab0f8248f18df2
Fleege <76089935+pfleege@users.noreply.github.com>
2024-07-31 09:05:01 +0300

    ADD functionality to IndividualBlog component
    - Add styling to individual blog page

--- src/components/IndividualBlog.jsx
+++ src/components/IndividualBlog.jsx
@@ -1,10 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const IndividualBlog = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    isLoading,
+    error,
+  } = useFetch(`http://localhost:8000/blogs/${id}`);
   return (
     <div className="individualBlog">
-      <h2>Current Blog is {id}</h2>
+      <div className="home">
+        {error && <p>{error}</p>}
+        {isLoading && <p>Loading data...</p>}
+        {blog && (
+          <article>
+            <h2>{blog.title}</h2>
+            <p>
+              <small>{blog.author}</small>
+            </p>
+            <p>{blog.blogText}</p>
+            <div className="goToHomePage">
+              <Link to="/">Back to list...</Link>
+            </div>
+          </article>
+        )}
+      </div>
     </div>
   );
 };
